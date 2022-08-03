480de0f05669bcfc4a5456dc8a4ea22a21e2a80f
yibane-yibane <85881925+yibane-yibane@users.noreply.github.com>
2022-08-03 15:30:41 +0300

    Update FlaskConfig.py

--- flask_app/FlaskConfig.py
+++ flask_app/FlaskConfig.py
@@ -1,6 +1,6 @@
 import os
 
-
+#fgds
 class FlaskConfig:
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI",
                                         "postgresql://postgres:test123@localhost:5432/postgres")
